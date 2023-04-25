7594bcfaebdb80e992256dd4226d17785f2b9c51
Andrea <zorzi.andrea@outlook.com>
2023-04-25 22:18:01 +0200

    False

--- src/components/Colors.jsx
+++ src/components/Colors.jsx
@@ -3,12 +3,7 @@ import Color from "./Color"
 export default function Colors() {
     return (
         <div className="colors container-fluid">
-            <Color/>
-            <Color/>
-            <Color/>
-            <Color/>
-            <Color/>
-            <Color/>
+
         </div>
     )
 }
