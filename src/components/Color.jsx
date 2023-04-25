2eb2f3c8ebd1fab0c010f3e1bd4275e50a070505
Andrea <zorzi.andrea@outlook.com>
2023-04-25 22:18:03 +0200

    Revert "False"
    This reverts commit 7594bcfaebdb80e992256dd4226d17785f2b9c51.

--- src/components/Color.jsx
+++ src/components/Color.jsx
@@ -1,10 +1,5 @@
 export default function Color() {
     return (
-        <div className="color">
-            <div className="content">
-                <p>#1929dv</p>
-                <span className="material-symbols-outlined">content_copy</span>
-            </div>
-        </div>
+        <div className="color"></div>
     )
 }
--- src/components/Colors.jsx
+++ src/components/Colors.jsx
@@ -3,7 +3,12 @@ import Color from "./Color"
 export default function Colors() {
     return (
         <div className="colors container-fluid">
-
+            <Color/>
+            <Color/>
+            <Color/>
+            <Color/>
+            <Color/>
+            <Color/>
         </div>
     )
 }
